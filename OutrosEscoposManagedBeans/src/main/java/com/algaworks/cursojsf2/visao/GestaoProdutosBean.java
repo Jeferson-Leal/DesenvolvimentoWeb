3e803b8930bf730a447ce1e96b54e6a023edf87e
Jeferson-Leal <jeferson.gleal@outlook.com>
2022-03-13 16:31:19 -0300

    Upando exercicio outros escopos managed beans

---
+++ OutrosEscoposManagedBeans/src/main/java/com/algaworks/cursojsf2/visao/GestaoProdutosBean.java
@@ -0,0 +1,50 @@
+package com.algaworks.cursojsf2.visao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.algaworks.cursojsf2.dominio.Produto;
+
+@ManagedBean
+@ViewScoped
+public class GestaoProdutosBean implements Serializable{
+
+	private List<Produto> produtos;
+	private Produto produto;
+	
+	public GestaoProdutosBean() {
+		this.produtos = new ArrayList<Produto>();
+		this.produto = new Produto();
+	}
+
+	public void incluir() {
+		this.produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+	
+	@PostConstruct
+	public void inicializar() {
+		System.out.println("Inicializou Bean");
+	}
+
+	@PreDestroy
+	public void finalizar() {
+		System.out.println("Finalizou Bean");
+	}
+	
+	
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+}
